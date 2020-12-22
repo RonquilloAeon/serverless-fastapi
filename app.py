010aa3800d069319064ddd0a2ff10b3b1f630d68
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-12-21 20:39:48 -0600

    Add exception handler and claims middleware

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from mangum import Mangum
 from starlette.requests import Request
 
 app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
+
+
+@app.middleware("http")
+async def get_claims(request: Request, call_next):
+    try:
+        ctx = request.scope["aws.event"].get("requestContext")
+        request.state.claims = ctx["authorizer"]["claims"]
+    except KeyError:
+        return JSONResponse(
+            status_code=422,
+            content={"message": "Missing authorization claims"},
+        )
+    else:
+        return await call_next(request)
 
 
 @app.get("/health")
@@ -12,13 +35,13 @@ def root() -> dict:
 
 @app.get("/me")
 def get_me(request: Request) -> dict:
-    event = request.scope["aws.event"]
-    authorizer = event["requestContext"]["authorizer"]["claims"]
+    claims = request.state.claims
 
     return {
-        "name": authorizer["name"],
-        "email": authorizer["email"],
-        "phone_number": authorizer["phone_number"],
+        "id": claims["cognito:username"],
+        "name": claims["name"],
+        "email": claims["email"],
+        "phone_number": claims["phone_number"],
     }
 
 
